2caa3507bb0d4ef060240e56e783f5523ed07de3
Rizky Iskandar <43482584+rbiskandar@users.noreply.github.com>
2022-08-05 15:03:07 +0700

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <p className="footer-p">Check out the code at <a href="https://github.com/Maherukh/MusikPlayer">✔ My Github Repo</a></p>
+        <p className="footer-p"> </p>
     );
 };
 
