98ed29b7bf0b4d1ebbd11010ff99777b5f3962a9
Rizky Iskandar <43482584+rbiskandar@users.noreply.github.com>
2022-08-05 16:11:12 +0700

    Add files via upload

--- src/components/Song.js
+++ src/components/Song.js
@@ -5,7 +5,7 @@ const Song = ({currentSong}) => {
         <div className="song-container">
             <img src={currentSong.cover} alt="studio album"></img>
             <h2>{currentSong.name}</h2>
-            <h2>{currentSong.arti}</h2>
+            <h3>{currentSong.arti}</h3>
             <h3>{currentSong.artist}</h3>
         </div>
     );
